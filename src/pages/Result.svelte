cad5322fb98764fdf0c1def26a4ec6325dda063d
chamegashi <msymsy0693wrb@gmail.com>
2021-12-18 03:00:05 +0900

    remake result

--- src/pages/Result.svelte
+++ src/pages/Result.svelte
@@ -19,46 +19,46 @@
 </script>
 
 <main class="bg-fixed h-screen" style="background-image: url(resultBack.jpg)">
-  <div class="bg-fixed h-screen" style="background-image: url(fubuki.gif)">
-    <div class="text-center text-4xl p-3 text-white font-semibold">あなたの今年のおすすめ！</div>
+  <div class="bg-fixed" style="background-image: url(fubuki.gif)">
+    <div class="text-center text-3xl p-3 text-white font-semibold">あなたの今年のおすすめ！</div>
     <div class="flex-wrap flex justify-center">
   
-      <div class="my-4 mx-3 w-5/12 h-60">
+      <div class="mb-4 mx-2 w-5/12 h-60">
         <div class="text-center m-2 text-xl text-white font-semibold ">原神オススメ</div>
         <div class="rounded border bg-white">
           <div class="flex justify-center">
-            <img class="object-cover m-3 w-10/12 h-40" src={"genshin/" + showCharactor.file} alt={showCharactor.name} />
+            <img class="object-cover m-3 w-10/12 h-40 rounded" src={"genshin/" + showCharactor.file} alt={showCharactor.name} />
           </div>
           <div class="text-xl text-center font-semibold pb-1">{showCharactor.name}</div>
         </div>
       </div>
     
-      <div class="my-4 mx-3 w-5/12 h-60">
+      <div class="mb-4 mx-2 w-5/12 h-60">
         <div class="text-center m-2 text-xl text-white font-semibold ">武器オススメ</div>
         <div class="rounded border bg-white">
           <div class="flex justify-center">
-            <img class="object-contain m-3 w-10/12 h-40" src={"weapon/" + showWeapon.file} alt={showWeapon.name} />
+            <img class="object-contain m-3 w-10/12 h-40 rounded" src={"weapon/" + showWeapon.file} alt={showWeapon.name} />
           </div>
           <div class="text-xl text-center font-semibold pb-1">{showWeapon.name}</div>
         </div>
       </div>
   
-      <div class="my-4 mx-3 w-11/12 h-60">
+      <div class="my-2 mx-3 w-11/12">
         <div class="text-center m-2 text-xl text-white font-semibold ">プロセカオススメ</div>
         <div class="rounded border bg-white">
           <div class="flex justify-center">
-            <img class="object-contain m-3 w-10/12 h-48" src={"proseka/" + showProseka.file} alt={showProseka.name} />
+            <img class="object-contain w-10/12 h-44 rounded" src={"proseka/" + showProseka.file} alt={showProseka.name} />
           </div>
         </div>
       </div>
   
-      <div class="my-4 mx-3 w-10/12 h-60">
+      <div class="mx-3 w-10/12 h-60">
         <div class="text-center m-2 text-xl text-white font-semibold ">ボカロオススメ</div>
         <div class="rounded border bg-white">
           <div class="flex justify-center m-2">
             <iframe 
               width="400"
-              height="200"
+              height="170"
               src={showYoutube.url}
               title="YouTube video player"
               frameborder="0"
